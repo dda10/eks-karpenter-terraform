f9a45980a17694a1715c99fe4390041a5c8588be
dda10 <ddaduyanh123@gmail.com>
2025-11-12 14:50:09 +0700

    change subnet cidr

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -11,7 +11,7 @@ data "aws_caller_identity" "current" {}
 locals {
   region = "us-east-1"
 
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = "11.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 # VPC Module
